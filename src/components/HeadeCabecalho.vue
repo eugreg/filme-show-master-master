de939893b9fbbf98fd4e0f583e358dfc4e74b9e2
eugreg <daiaegregory@gmail.com>
2022-11-27 20:23:54 -0300

    27/11

--- src/components/HeadeCabecalho.vue
+++ src/components/HeadeCabecalho.vue
@@ -26,6 +26,9 @@ export default {
     go() {
       this.$router.push(`/filmes_por_genero/${this.genero}`);
     },
+    getProfileUrl(avatar_path) {
+      return `https://image.tmdb.org/t/p/w500${avatar_path}`;
+    },
   },
 };
 </script>
@@ -55,9 +58,14 @@ export default {
       </div>
       <div>
         <RouterLink to="/minha-conta"
-          ><span class="cabecalho-span">{{
-            userData.username
-          }}</span></RouterLink
+          ><span class="cabecalho-span">
+           <img
+          class="poster-profile-header"
+          :src="getProfileUrl(userData.avatar.tmdb.avatar_path)"
+          alt="linda imagem do avatar"
+    
+        /> 
+          </span></RouterLink
         >
         <button>
           <RouterLink to="/pesquisa" class="button-pesquisar"
